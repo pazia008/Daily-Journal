e7da4cf35788ab6695ce2a8b6cb5ed8714b9beec
pazia008 <pazia008@gmail.com>
2021-01-06 14:16:53 -0600

    made journal entries appear

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,20 +1,17 @@
-import { useJournalEntries } from "./JournalDataProvider.js"
+import { useJournalEntries, getEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
 // DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("main")
+const entryLog = document.querySelector(".entryLog")
 
 export const EntryListComponent = () => {
     // Use the journal entry data from the data provider component
-
-    const entries = useJournalEntries()
-
-    for (const entry of entries) {
-        const entryHTML = JournalEntryComponent(entry)
-            /*
-                Invoke the component that returns an
-                HTML representation of a single entry
-            */
-        entryLog.innerHTML += entryHTML
-    }
+    getEntries()
+        .then(() => {
+            let useEntry = useJournalEntries()
+            for (const entry of useEntry) {
+                const entryHTML = JournalEntryComponent(entry)
+                entryLog.innerHTML += entryHTML
+            }
+        })
 }
